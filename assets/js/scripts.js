fabd1e8a86c5f88f2c89b20286799b01e2910f37
Tolu Adesina <tadesina90@gmail.com>
2020-01-30 14:30:15 +0100

    Add a cancel button to the drawer

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -1,8 +1,25 @@
-$().ready(() => {
-  console.log('we are ready');
+// JavaScript to make all interactions work
 
-  $('.mobile-menu').click(() => {
-    console.log('You just clicked me');
-  });
+(() => {
+  console.log("We are ready!");
 
-});
+  const nav = document.querySelector(".mobile-menu");
+  const drawer = document.querySelector(".nav-drawer");
+  let isClicked = false;
+  if (!isClicked) {
+    nav.onclick = function() {
+      console.log("I am opening");
+      isClicked = true;
+      nav.classList.add("isClose");
+      drawer.classList.add("isActive");
+    };
+  }
+
+  if (isClicked) {
+    const close = document.querySelector(".isClose");
+    close.onclick = function() {
+      console.log("I am closing");
+      drawer.classList.remove("isActive");
+    };
+  }
+})();
