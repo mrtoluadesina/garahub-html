3d1c100719163e00a12c56df91421577386f3d6f
Tolu Adesina <tadesina90@gmail.com>
2020-02-01 18:21:14 +0100

    add scripts to add and remove class to show and hide the add to cart section

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -6,6 +6,9 @@
   const nav = document.querySelector(".mobile-menu");
   const drawer = document.querySelector(".nav-drawer");
   const close = document .querySelector(".close-menu");
+  const showAddToCart = document.querySelector("#showAddToCart");
+  const addToCart = document.querySelector(".add-to-cart");
+  const added = document.querySelector('#addToCart');
 
   // Open and close the drawer
   nav.addEventListener('click', () => {
@@ -15,7 +18,15 @@
     drawer.classList.remove('isActive');
   });
 
-  // Next function
+  // show add to card on product card
+  showAddToCart.addEventListener('click', () => {
+    showAddToCart.classList.add('isClicked');
+    addToCart.classList.add('isActive');
+  });
+  added.addEventListener('click', () => {
+    addToCart.classList.remove('isActive');
+    showAddToCart.classList.remove('isClicked');
+  })
   
 
 })();
